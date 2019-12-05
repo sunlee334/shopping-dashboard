d403a2a01218f9abee977769c911895d845eae36
Sun Lee <sunlee334@gmail.com>
2019-12-05 16:38:37 +0900

    사용하지 않는 메뉴 제거

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LineChartComponent } from './line-chart/line-chart.component';
-import { BarChartComponent } from './bar-chart/bar-chart.component';
-import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
-import { RadarChartComponent } from './radar-chart/radar-chart.component';
-import { PieChartComponent } from './pie-chart/pie-chart.component';
-import { BubbleChartComponent } from './bubble-chart/bubble-chart.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ConversionDashboardComponent } from './conversion-dashboard/conversion-dashboard.component';
 import { BehaviorDashboardComponent } from './behavior-dashboard/behavior-dashboard.component';
@@ -15,12 +9,6 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'conversion-dashboard', component: ConversionDashboardComponent },
   { path: 'behavior-dashboard', component: BehaviorDashboardComponent },
-  { path: 'line-chart', component: LineChartComponent },
-  { path: 'bar-chart', component: BarChartComponent },
-  { path: 'doughnut-chart', component: DoughnutChartComponent },
-  { path: 'radar-chart', component: RadarChartComponent },
-  { path: 'pie-chart', component: PieChartComponent },
-  { path: 'bubble-chart', component: BubbleChartComponent },
 ];
 
 @NgModule({
